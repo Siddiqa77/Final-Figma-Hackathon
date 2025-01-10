4c1c1dcca0871d8d62994ab33264f0e46a054844
Siddiqa Badar <siddiqaasim778899@gmail.com>
2025-01-11 00:18:12 +0500

    newchange

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         domains: ['cdn.sanity.io'],
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
